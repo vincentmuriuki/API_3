d6e8f6ebf3d2d797c422ae901bc6fa65e5f7f3cd
vincentmuriuki <vincentmuriuki42@gmail.com>
2018-11-19 01:33:11 +0300

    [Chore #162039340] Write tests for the  end point - This is the endpoint that retrieves all orders

---
+++ app/tests/v1/test_orders.py
@@ -0,0 +1,33 @@
+# unittest
+import unittest
+
+# from run import app
+from run import app
+import json
+
+from app import create_app
+
+class TestOrders(unittest.TestCase):
+    """
+    This class contains tests for all the endpoints
+    """
+    def setUp(self):
+        # app.testing = True
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+    
+    def test_get_orders(self):
+        response = self.client.get(
+            "/api/v1/orders",
+            headers={"content-type": "application/json"}
+        )
+        
+        self.assertEqual(response.status_code, 200)
+    def tearDown(self):
+        self.app_context.pop()
+        
