2bf66d25949d2a95d854cbd3b93f36a66ddbaf98
vincentmuriuki <vincentmuriuki42@gmail.com>
2018-11-18 22:54:46 +0300

    [Feature #162038598] - Add  endpoint - This retrieves a list of all customer orders

---
+++ app/api/v1/models/orders.py
@@ -0,0 +1,25 @@
+
+from flask import jsonify
+orders = []
+
+
+class CustomerOrders():
+
+
+    def __init__(self):
+        self.orders = orders
+
+    def save(self, name, price, quantity):
+        order_accompanment = {
+            'order_id': len(orders) + 1,
+            'Description': {'Name of Snack' : name},
+            'Price': price,
+            'Quantity': quantity
+        }
+        
+        self.orders.append(order_accompanment)
+
+        return self.orders
+
+    def retrieveorders(self):
+        return self.orders
